0332bf069c9d8f94820973b05bbb5561c6f00f55
chany <feeble19@naver.com>
2021-08-16 11:14:50 +0900

    문자열, 수식

---
+++ day0816/src/Calculate.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class Calculate {
+	public static void main(String[] args) {
+		System.out.println("2021-8-16");
+		System.out.println(2021-8-16);
+		
+		//Q : 5000원짜리 자장면 3그릇의 가격은 얼마인가요?
+		// - 사칙연산은 +, -, *, / 로 표시합니다.
+		System.out.println("15000");
+		System.out.println(15000);
+		System.out.println(5000*3);
+	}
+}
