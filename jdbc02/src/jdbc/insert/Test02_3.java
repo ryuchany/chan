e2601bec553791b17b76b7a6682a4d3231128109
chany <feeble19@naver.com>
2021-10-04 14:53:33 +0900

    자동커밋 예제(사용안함)

---
+++ jdbc02/src/jdbc/insert/Test02_3.java
@@ -0,0 +1,38 @@
+package jdbc.insert;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Test02_3 {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("메뉴 이름 : ");
+		String menuName = sc.nextLine();
+		System.out.print("메뉴 가격 : ");
+		int menuPrice = sc.nextInt();
+		sc.nextLine();
+		System.out.print("메뉴 유형 : ");
+		String menuType = sc.nextLine();
+		sc.close();
+
+		//Q : 위 변수에 저장된 값이 menu 테이블에 insert 될 수 있도록 JDBC 구문을 작성해보세요.
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection con = DriverManager.getConnection(
+										"jdbc:oracle:thin:@localhost:1521:xe", "kh", "kh");
+		con.setAutoCommit(false);//자동커밋 해제
+
+		String sql = "insert into menu(menu_name, menu_price, menu_type) "
+				+ "values('"+menuName+"', "+menuPrice+", '"+menuType+"')";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.execute();
+
+		con.rollback();
+//		con.commit();
+
+		con.close();
+		System.out.println("등록 완료");
+	}
+}
