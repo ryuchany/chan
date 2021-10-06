e0552bc8fb0a674b801ba3b93ec34296e00da3be
chany <feeble19@naver.com>
2021-10-06 10:23:09 +0900

    회원 아이디 또는 닉네임 검사

--- jdbc03/src/jdbc/select/Test08.java
+++ jdbc03/src/jdbc/select/Test08.java
@@ -7,41 +7,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Test08 {
-	public static void main(String[] args) throws Exception  {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+//		select * from member where member_id like '%'||?||'%' or member_nick like '%'||?||'%';
+//		select * from member where instr(member_id, ?) > 0 or instr(member_nick, ?) > 0;
 
-		//입력
-		String keyword = "zxc";
+//		입력
+		String keyword = "2";
 
-		//처리
+//		처리
 		Class.forName("oracle.jdbc.OracleDriver");
 		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "kh", "0000");
-	
+
+//		String sql = "select * from member where member_id like '%'||?||'%' or member_nick like '%'||?||'%';";
 		String sql = "select * from member where instr(member_id, ?) > 0 or instr(member_nick, ?) > 0";
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setString(1, keyword);
 		ps.setString(2, keyword);
 		ResultSet rs = ps.executeQuery();
 
-		while(rs.next()) {
+		while (rs.next()) {
 			System.out.print(rs.getString("member_id"));
 			System.out.print(" / ");
-//			System.out.print(rs.getString("member_pw"));
-//			System.out.print(" / ");
 			System.out.print(rs.getString("member_nick"));
 			System.out.print(" / ");
-			System.out.print(rs.getDate("member_birth"));
-			System.out.print(" / ");
-			System.out.print(rs.getString("member_email"));
-			System.out.print(" / ");
-			System.out.print(rs.getString("member_phone"));
-			System.out.print(" / ");
-			System.out.print(rs.getDate("member_join"));
-			System.out.print(" / ");
-			System.out.print(rs.getString("member_point"));
-			System.out.print(" / ");
-			System.out.println(rs.getString("member_grade"));
+			System.out.print(rs.getString("member_grade"));
+			System.out.println();
 		}
 
 		con.close();
+
 	}
 }
