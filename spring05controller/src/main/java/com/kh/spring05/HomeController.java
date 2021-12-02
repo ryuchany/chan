9ddc77f34ca6fa91df377a41b473f67b04bfe41f
chany <feeble19@naver.com>
2021-12-03 03:35:02 +0900

    spring05

--- spring05controller/src/main/java/com/kh/spring05/HomeController.java
+++ spring05controller/src/main/java/com/kh/spring05/HomeController.java
@@ -1,39 +1,20 @@
 package com.kh.spring05;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
-/**
- * Handles requests for the application home page.
- */
-@Controller
-public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+public class HomeController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("a", 10);//a=10
+		mv.addObject("b", 200);//b=200
+		mv.setViewName("home");
+		return mv;
 	}
-	
+
 }
--- spring05controller/src/main/webapp/WEB-INF/views/home.jsp
+++ spring05controller/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,5 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
-</body>
-</html>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<h1>home</h1>
+<h1>a = ${a}, b = ${b}</h1>
+<h1>a = <%=request.getAttribute("a") %>, b = <%=request.getAttribute("b") %></h1>
